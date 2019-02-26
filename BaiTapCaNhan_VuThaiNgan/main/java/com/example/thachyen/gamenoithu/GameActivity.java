cc9483495d7cf9f89489286c37aec88e13b6903f
ThaiNgan1712 <vuthaingan1712@gmai.com>
2019-02-26 08:48:23 +0700

    sad

---
+++ BaiTapCaNhan_VuThaiNgan/main/java/com/example/thachyen/gamenoithu/GameActivity.java
@@ -0,0 +1,13 @@
+package com.example.thachyen.gamenoithu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game_layout);
+    }
+}
